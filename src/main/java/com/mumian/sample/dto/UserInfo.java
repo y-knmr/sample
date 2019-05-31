22f7422b18819f31fbfdc41e78243e6b4d83c318
y-knmr <y-knmr@nttpc.co.jp>
2019-05-31 11:54:59 +0900

    てすと

--- src/main/java/com/mumian/sample/dto/UserInfo.java
+++ src/main/java/com/mumian/sample/dto/UserInfo.java
@@ -7,21 +7,25 @@ import javax.inject.Named;
 @ApplicationScoped
 public class UserInfo {
 
+	/** ユーザ名 */
 	private String userName;
+	/** パスワード */
 	private String password;
-	
+
 	public String getUserName() {
 		return userName;
 	}
+
 	public void setUserName(String userName) {
 		this.userName = userName;
 	}
+
 	public String getPassword() {
 		return password;
 	}
+
 	public void setPassword(String password) {
 		this.password = password;
 	}
 
-	
 }
